e2c6b3364ce45142ea031be82b926de8c04e6408
Terrtia <or1994@hotmail.fr>
2016-11-23 23:12:35 +0100

    replacement des fichiers de tests

---
+++ core/src/test/java/com/mygdx/game/models/elements/AlienTest.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.models.elements;
+
+/**
+ * Created by aurelien on 23/11/16.
+ */
+public class AlienTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+}
